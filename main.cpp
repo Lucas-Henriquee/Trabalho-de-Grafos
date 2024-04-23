6c2acf83bf2730175331b58905e216a218622807
BrenoMontanha <breno.montanha@estudante.ufjf.br>
2024-04-23 11:55:10 -0300

    Inicio da impletação da main
    Co-authored-by: Lucas Henrique Nogueira <Lucas-Henriquee@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include "include/Graph.hpp"
+using namespace std;
 
-
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+    
+    /*
+    argv[1] arquivo de entrada
+    argv[2] arquivo de saida
+    argv[3] Se é direcionado ou não
+    argv[4] Se tem peso na aresta
+    argv[5] Se tem peso nos nós
+    */
+
+    if (argc != 0)
+    {
+        string filename = argv[1];
+        ifstream file(filename);
+
+        if (file.is_open()){
+            Graph *a = new Graph(file);
+            a->print_graph();
+        }
+        else
+            cout << "Erro ao abri o arquivo" << endl;
+        // menu();
+        file.close();
+    }
     return 0;
 }
--- source/Graph.cpp
+++ source/Graph.cpp
@@ -1,7 +1,17 @@
 #include "../include/Graph.hpp"
-
-Graph::Graph(std::ifstream &instance){
-
+using namespace std;
+Graph::Graph(std::ifstream &instance)
+{
+    /*
+    Recebe o arquivo aberto
+    coloca numero de nós igual a 0
+    Itera pelas linhas, em cada linha:
+        verifica se o primeiro nó está na lista duplamente encadeada
+            se não estiver cria o nó
+        verifica se o segundoo nó está na lista duplamente encadeada
+            se não estiver cria o nó
+        cria a aresta da linha
+    */
 }
 
 Graph::Graph()
@@ -37,6 +47,7 @@ void Graph::add_node(size_t node_id, float weight)
         aux->_previous_node = _last;
         _last = aux;
     }
+    _number_of_nodes++;
 }
 
 void Graph::add_edge(size_t node_id_1, size_t node_id_2, float weight)
@@ -47,60 +58,57 @@ void Graph::add_edge(size_t node_id_1, size_t node_id_2, float weight)
     for (size_t i = 0; i < _number_of_nodes; i++)
     {
         if (aux_node->_id == node_id_1)
-        {
             search_node_1 = aux_node;
-        }
         if (aux_node->_id == node_id_2)
-        {
             search_node_2 = aux_node;
-        }
         if (search_node_1 != NULL && search_node_2 != NULL)
-        {
             break;
-        }
     }
     if (search_node_1 == NULL || search_node_2 == NULL)
-    {
         return;
-    }
+    Edge *new_edge_1 = new Edge;
+    new_edge_1->_target_id = node_id_2;
+    new_edge_1->_weight = weight;
+    new_edge_1->_next_edge = NULL;
+    search_node_1->_number_of_edges = +1;
     if (search_node_1->_first_edge != NULL)
     {
         Edge *aux_edge = search_node_1->_first_edge;
-        for (; aux_edge->_next_edge != NULL; aux_edge->_next_edge){}
-        Edge *new_edge = new Edge;
-        new_edge->_target_id = node_id_2;
-        new_edge->_weight = weight;
-        new_edge->_next_edge = NULL;
-        aux_edge->_next_edge = new_edge;
+        for (; aux_edge->_next_edge != NULL; aux_edge->_next_edge)
+        {
+        }
+        aux_edge->_next_edge = new_edge_1;
     }
-    else{
-        Edge *new_edge = new Edge;
-        new_edge->_target_id = node_id_2;
-        new_edge->_weight = weight;
-        new_edge->_next_edge = NULL;
-        search_node_1->_first_edge = new_edge
-    }
-    if (_directed && search_node_2->_first_edge != NULL)
+    else
+        search_node_1->_first_edge = new_edge_1;
+    if (!_directed)
     {
-        Edge *aux_edge = search_node_2->_first_edge;
-        for (; aux_edge->_next_edge != NULL; aux_edge->_next_edge){}
-        Edge *new_edge = new Edge;
-        new_edge->_target_id = node_id_1;
-        new_edge->_weight = weight;
-        new_edge->_next_edge = NULL;
-        aux_edge->_next_edge = new_edge;
-    }
-    else{
-        Edge *new_edge = new Edge;
-        new_edge->_target_id = node_id_1;
-        new_edge->_weight = weight;
-        new_edge->_next_edge = NULL;
-        search_node_2->_first_edge = new_edge
+        Edge *new_edge_2 = new Edge;
+        new_edge_2->_target_id = node_id_1;
+        new_edge_2->_weight = weight;
+        new_edge_2->_next_edge = NULL;
+        search_node_2->_number_of_edges = +1;
+        if (search_node_2->_first_edge != NULL)
+        {
+            Edge *aux_edge = search_node_2->_first_edge;
+            for (; aux_edge->_next_edge != NULL; aux_edge->_next_edge)
+            {
+            }
+            aux_edge->_next_edge = new_edge_2;
+        }
+        else
+            search_node_2->_first_edge = new_edge_2;
     }
 }
 
 void Graph::print_graph()
 {
+    for (Node *node = _first; node != NULL; node = node->_next_node){
+        cout << node->_id;
+        for (Edge *edge = node->_first_edge; edge != NULL; edge = edge->_next_edge)
+            cout << " -> " << edge->_target_id;
+    }  
+    cout << endl;
 }
 
 void Graph::print_graph(std::ofstream &output_file)
